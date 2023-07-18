3627872e7955c048fd80effa60f47a50f5a4e8c7
Deekshitha2024 <130962251+Deekshitha2024@users.noreply.github.com>
2023-07-18 11:08:42 +0530

    Create HashCodeSum.java

---
+++ HashCodeSum.java
@@ -0,0 +1,20 @@
+public class HashCodeSum {
+    public static int customHashFunction(String input){
+        int hashCode=0;
+        for(int i=0;i<input.length();i++){
+            hashCode += input.charAt(i);
+        }
+        return hashCode;
+    }
+    public static void main(String args[]){
+        String name="Deeksha";
+        int hashCode=customHashFunction(name);
+        System.out.println(name);
+        System.out.println(hashCode);
+        
+            
+        
+
+    }
+    
+}
